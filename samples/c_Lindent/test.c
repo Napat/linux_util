f3f21cfb3450f209ee0da7efc814e629a3deda06
Napat Rungruangbangchan <napat.joe@gmail.com>
2017-04-14 16:35:59 +0700

    samples project of Lindent

---
+++ samples/c_Lindent/test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//test1
+// test2
+
+
+
+/********************************
+  Multiline comment
+****************************/
+
+void test()
+{
+printf("%s()\r\n", __FUNCTION__);
+}
+
+int main()
+{
+printf("hahaha\r\n");
+ test();
+   return 0;
+
+}
